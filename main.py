bd0c71d0e57e03ddbad52fbca484c5c21775105e
Solarrixs <ma.xxy11@protonmail.com>
2024-02-10 18:42:54 -0500

    Finished

--- main.py
+++ main.py
@@ -1,5 +1,37 @@
 from rcsbsearchapi.search import TextQuery
+from rcsbsearchapi.const import CHEMICAL_ATTRIBUTE_SEARCH_SERVICE, STRUCTURE_ATTRIBUTE_SEARCH_SERVICE
+from rcsbsearchapi.search import AttributeQuery
+from rcsbsearchapi import rcsb_attributes as attrs
+
+# Create terminals for each query
+q1 = TextQuery("heat-shock transcription factor")
+q2 = attrs.rcsb_struct_symmetry.symbol == "C2"
+q3 = attrs.rcsb_struct_symmetry.kind == "Global Symmetry"
+q4 = attrs.rcsb_entry_info.polymer_entity_count_DNA >= 1
+
+# combined using bitwise operators (&, |, ~, etc)
+query = q1 & (q2 & q3 & q4)
+
+# Call the query to execute it
+for assemblyid in query("assembly"):
+    print(assemblyid)
+
+# By default, service is set to "text" for structural attribute search
+q1 = AttributeQuery("exptl.method", "exact_match", "electron microscopy",
+                    STRUCTURE_ATTRIBUTE_SEARCH_SERVICE # this constant specifies "text" service
+                    )
+
+# Need to specify chemical attribute search service - "text_chem"
+q2 = AttributeQuery("drugbank_info.brand_names", "contains_phrase", "tylenol",
+                    CHEMICAL_ATTRIBUTE_SEARCH_SERVICE # this constant specifies "text_chem" service
+                    )
+
+query = q1 & q2
+
+list(query())
 
 q1 = TextQuery("hemoglobin")
 for idscore in list(q1(results_verbosity="verbose")):
     print(idscore)
+
+q1.count()
